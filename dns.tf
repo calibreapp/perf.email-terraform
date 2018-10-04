f1b61dc65aa42019ede749ed5e92ddeca6a7e902
Ben Schwarz <ben@germanforblack.com>
2018-10-04 20:21:50 +1000

    Point at netlify

--- dns.tf
+++ dns.tf
@@ -1,15 +1,15 @@
 resource "dnsimple_record" "cname-cloudfront" {
-    domain = "perf.email"
-    name = "www"
-    value = "debqn5w4zz8r9.cloudfront.net"
-    type = "CNAME"
-    ttl = "300"
+  domain = "perf.email"
+  name   = "www"
+  value  = "perfemail.netlify.com"
+  type   = "CNAME"
+  ttl    = "300"
 }
 
 resource "dnsimple_record" "alias-cloudfront" {
-    domain = "perf.email"
-    name = ""
-    value = "debqn5w4zz8r9.cloudfront.net"
-    type = "ALIAS"
-    ttl = "300"
+  domain = "perf.email"
+  name   = ""
+  value  = "perfemail.netlify.com"
+  type   = "ALIAS"
+  ttl    = "300"
 }
